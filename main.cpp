6f89aa3fefc4c4e668d9494718b99deef6ab7168
250152 <250152@newuu.uz>
2025-09-11 12:43:19 +0500

    week3_2

---
+++ main.cpp
@@ -0,0 +1,44 @@
+  #include <iostream>
+   using namespace std;
+//problem11
+//    int main() {
+//    double x=1.31e3, y=411e-4;
+//    cout<<x<<" "<<y<<endl;
+// }
+//problem13
+// int main() {
+//    float area,side;
+//     cout<<"side length=";
+//     cin >>side;
+//     area=((3*(1.73)/2))*side*side;
+//     cout<<"area="<<area<<endl;
+//     return 0;
+// }
+//problem14
+// int main() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     num++;
+//     num*=3;
+//     num-=14;
+//     num/=4;
+//     num--;
+//     num%=9;
+//     cout << num << endl;
+//     return 0;
+// }
+//problem17
+// int main() {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << (a==b)<< endl;
+//     return 0;
+// }
+//problem16
+// int main() {
+//     float x=17, y=1;
+//     float z=(x+(y+x))/(y+y+y)*(y+y);
+//     cout<<"x="<<x<<endl;
+// }
+
