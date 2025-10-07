ca343321475d3a2caf5dcdd789f76e14ad107884
TheNurshod <250152@newuu.uz>
2025-10-08 01:29:24 +0500

    looping

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+// int main() {
+//     int sum = 0;
+//     for (int i = 1; i <=10; i++) {
+//         cout<<i<<"";
+//         sum += i;
+//     }
+//     cout<<endl<<sum<<endl;
+//     return 0;
+// }
+
+//PROBLEM 2
+
+// int main() {
+//     int i = 1;
+//     while (i <= 100) {
+//         if (i % 5 != 0) {
+//             cout << i << " ";
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+//PROBLEM 4
+
+// int main() {
+//     int i = 1, n;
+//     cin >> n;
+//     cout <<"Enter n': "<< endl;
+//     while (i <= n) {
+//         if (i % 10 == 0) {
+//             cout << i << " ";
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+// PROBLEM 5
+// int main() {
+//     int count = 0;
+//     for (char c = 'A'; c <= 'Z'; c++) {
+//         cout << c << " ";
+//         count++;
+//         if (count % 5 == 0) {
+//             cout << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+//problem 8
+
+//
+// int main() {
+//     int n, rev = 0;
+//     cin >> n;
+//
+//     while (n > 0) {
+//         int digit = n % 10;
+//         rev = rev * 10 + digit;
+//         n = n / 10;
+//     }
+//
+//     cout << rev;
+//     return 0;
+// }
