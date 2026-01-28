d552889455eb07aaa498e3f2683247844fd96677
250152 <250152@newuu.uz>
2026-01-28 13:34:25 +0500

    Week1_lab1

--- src/Week1_lab1.java
+++ src/Week1_lab1.java
@@ -1,105 +1,105 @@
-1st
-public class Mainn {
-    public static void main(String[] args){
-        System.out.println("Address: ");
-        System.out.println(Postcode );
-        System.out.println("being unemployed");
-    }
-
-}
-import java.util.Scanner;
- 3rd exercise
-public class Week1_lab1 {
-    public static void main(String[] args){
-        double height=7.9;
-        double width=4.5;
-        double area = height*width;
-        double perimeter= 2*(height*width);
-        System.out.printf("Peremeter: %.2f \n", perimeter);
-        System.out.printf("Area: %.2f ", area );
-
-    }
-
-}
-4th problem
-import java.util.Scanner;
-
-public class Week1_lab1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("please enter radius ");
-        double radius = input.nextDouble();
-        input.nextLine();
-        System.out.println("please enter length ");
-        double length  = input.nextDouble();
-        //double PI = 3.14;
-        double area = Math.PI *radius * radius;
-        double volume = area*length ;
-        System.out.printf("Volume: %.2f \n", volume );
-        System.out.printf("Area: %.2f ", area );
-
-    }
-
-}
-5th problem
-import java.util.Scanner;
-
-public class Week1_lab1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("please enter side ");
-        double side = input.nextDouble();
-        input.nextLine();
-        System.out.println("please enter length ");
-        double length  = input.nextDouble();
-
-        double area = (Math.sqrt(3)*side*side)/4;
-        double volume = area*length ;
-        System.out.printf("Volume: %.2f \n", volume );
-        System.out.printf("Area: %.2f ", area );
-
-    }
-
-}
-7th problem
-import java.util.Scanner;
-
-public class Week1_lab1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("please enter distance ");
-        double distance = input.nextDouble();
-        System.out.println("please enter miles per gallon");
-        double milespergallon = input.nextDouble();
-        System.out.println("please enter price per gallon");
-        double price = input.nextDouble();
-        double tripcost = (distance/milespergallon)*price;
-
-        System.out.printf("Cost of driving: %.2f " , tripcost);
-
-    }
-}
-8th problem
-import java.util.Scanner;
-
-public class Week1_lab1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("please enter number ");
-        int number = input.nextInt();
-        int sum=number/1000+(number%1000)/100+((number%1000)%100)/10 +number%10;
-        System.out.print(sum);
-    }
-}
-9th problem
-import java.util.Scanner;
-public class Week1_lab1 {
-    public static void main(String[] args) {
-        System.out.println("please enter 3 integers ");
-        int number = input.nextInt();
-    }
-}
+//1st
+//public class Mainn {
+//    public static void main(String[] args){
+//        System.out.println("Address: ");
+//        System.out.println(Postcode );
+//        System.out.println("being unemployed");
+//    }
+//
+//}
+//import java.util.Scanner;
+// 3rd exercise
+//public class Week1_lab1 {
+//    public static void main(String[] args){
+//        double height=7.9;
+//        double width=4.5;
+//        double area = height*width;
+//        double perimeter= 2*(height*width);
+//        System.out.printf("Peremeter: %.2f \n", perimeter);
+//        System.out.printf("Area: %.2f ", area );
+//
+//    }
+//
+//}
+//4th problem
+//import java.util.Scanner;
+//
+//public class Week1_lab1 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("please enter radius ");
+//        double radius = input.nextDouble();
+//        input.nextLine();
+//        System.out.println("please enter length ");
+//        double length  = input.nextDouble();
+//        //double PI = 3.14;
+//        double area = Math.PI *radius * radius;
+//        double volume = area*length ;
+//        System.out.printf("Volume: %.2f \n", volume );
+//        System.out.printf("Area: %.2f ", area );
+//
+//    }
+//
+//}
+//5th problem
+//import java.util.Scanner;
+//
+//public class Week1_lab1 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("please enter side ");
+//        double side = input.nextDouble();
+//        input.nextLine();
+//        System.out.println("please enter length ");
+//        double length  = input.nextDouble();
+//
+//        double area = (Math.sqrt(3)*side*side)/4;
+//        double volume = area*length ;
+//        System.out.printf("Volume: %.2f \n", volume );
+//        System.out.printf("Area: %.2f ", area );
+//
+//    }
+//
+//}
+//7th problem
+//import java.util.Scanner;
+//
+//public class Week1_lab1 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("please enter distance ");
+//        double distance = input.nextDouble();
+//        System.out.println("please enter miles per gallon");
+//        double milespergallon = input.nextDouble();
+//        System.out.println("please enter price per gallon");
+//        double price = input.nextDouble();
+//        double tripcost = (distance/milespergallon)*price;
+//
+//        System.out.printf("Cost of driving: %.2f " , tripcost);
+//
+//    }
+//}
+//8th problem
+//import java.util.Scanner;
+//
+//public class Week1_lab1 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("please enter number ");
+//        int number = input.nextInt();
+//        int sum=number/1000+(number%1000)/100+((number%1000)%100)/10 +number%10;
+//        System.out.print(sum);
+//    }
+//}
+//9th problem
+//import java.util.Scanner;
+//public class Week1_lab1 {
+//    public static void main(String[] args) {
+//        System.out.println("please enter 3 integers ");
+//        int number = input.nextInt();
+//    }
+//}
